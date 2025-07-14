ff1932e97dfa949f0ad49d2206fa7c9b0746b15a
Draco_Raiden <draco.raiden@gmail.com>
2025-07-14 18:35:30 +0500

    Project Complete

--- script.js
+++ script.js
@@ -1,14 +1,7 @@
-// old format------>>
-// json = fetchJSON(`/currencies/{fromCurrency}/{toCurrency}`)
-// rate = json[toCurrency]
-
-// new format ------->>>
-// json = fetchJSON(`/currencies/{fromCurrency}`)
-// rate = json[fromCurrency][toCurrency]
-
+// Base URLto calculate exchange rates from EUR to all Currencies
 BASE = "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/eur.json"
 
-
+// Intialization of all HTML elements needed during the execution of the Program
 let dropdowns = document.querySelectorAll("select");
 let rate = document.querySelector("#rate");
 let btn = document.querySelector("form button");
@@ -16,6 +9,7 @@ let fromCurrency = document.querySelector("#from select");
 let toCurrency = document.querySelector("#to select");
 let msg = document.querySelector("#msg p");
 
+// Adding all options in the dropdown menus
 for (let select of dropdowns) {
     for (let currCode in countryList) {
         let newOption = document.createElement("option");
@@ -35,7 +29,7 @@ for (let select of dropdowns) {
     });
 }
 
-
+// Updating flags properly according to the Currency Codes
 const updateFlag = (element) => {
     let currCode = element.value;
     let countryCode = countryList[currCode];
@@ -44,37 +38,37 @@ const updateFlag = (element) => {
     img.src = newSrc;
 }
 
-const updateRate = (element) => {
-    let currCode = element.value;
-    let countryCode = countryList[currCode];
-    let newRate = ``
-}
-
-btn.addEventListener("click", async (evt) => {
-    evt.preventDefault();
-    let amt = document.querySelector("#user-input input");
-    let amtValue = amt.value;
-
-    if (amtValue === "" || amtValue < 0) {
-        amtValue = 1;
-        amt.value = "1";
-    }
-
-    // console.log(fromCurrency.value.toLowerCase(), toCurrency.value.toLowerCase());
-
+// Function to calculate the updated rate
+const updateRate = async () => {
     let response = await fetch(BASE);
     let data = await response.json();
     let fromRate = data.eur[`${fromCurrency.value.toLowerCase()}`];
     let toRate = data.eur[`${toCurrency.value.toLowerCase()}`];
     let exrate = toRate / fromRate;
-
-    // console.log(`EUR to ${fromCurrency.value}:`, fromRate);
-    // console.log(`EUR to ${toCurrency.value4}:`, toRate);
-    // console.log(`Rate: ${exrate}`);
     exrate = exrate.toFixed(3);
+    return exrate;
+}
 
+// Check Amount Value and set to 1 if negative or empty
+const checkVal = (amount) => {
+    if (amount.value === "" || amount.value < 0) {
+        amount.value = "1";
+    }
+}
+
+// Update msg
+const updateMsg = (exrate, amt) => {
     rate.innerText = `1 ${fromCurrency.value} = ${exrate} ${toCurrency.value}`;
     let finalAmount = amt.value * exrate;
-    console.log(finalAmount);
-    msg.innerText = `${amtValue} ${fromCurrency.value} = ${finalAmount} ${toCurrency.value}`;
+    msg.innerText = `${amt.value} ${fromCurrency.value} = ${finalAmount} ${toCurrency.value}`;
+}
+
+// Event Listner to perform exchange
+btn.addEventListener("click", async (evt) => {
+    // Prevent page from refreshing
+    evt.preventDefault();
+    let amt = document.querySelector("#user-input input");
+    checkVal(amt);
+    let exrate = await updateRate();
+    updateMsg(exrate, amt);
 });
